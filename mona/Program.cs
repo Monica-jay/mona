00e619ef588a543433dfaae522f9bae8a1530f4f
Monica Jayakriba Lawrence <Mlawrence9155@conestogac.on.ca>
2020-10-09 04:15:12 +0530

    Commit for Assignment 1

--- mona/Program.cs
+++ mona/Program.cs
@@ -32,29 +32,5 @@ namespace mona
             } while (choice != "2");
             Console.ReadKey();
         }
-    }
-    public static class TriangleSolver
-    {
-        public static string Analyze(int sideA, int sideB, int sideC)
-        {
-            if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB)
-            {
-                Console.WriteLine("\nYes, It forms a Triangle...");
-                if (sideA == sideB && sideB == sideC && sideC == sideA)
-                {
-                    return "\nIt is an Equilateral Triangle\n";
-                }
-                if (sideA == sideB || sideB == sideC || sideC == sideA)
-                {
-                    return "\nIt is an Isosceles Triangle\n";
-                }
-                return "\nIt is an Scalene Triangle\n";
-            }
-            else
-            {
-                return "\nIt is not a triangle\n";
-            }
-
-        }
-    }
+    }   
 }
---
+++ mona/TriangleSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mona
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int sideA, int sideB, int sideC)
+        {
+            if (sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA > sideB)
+            {
+                Console.WriteLine("\nYes, It forms a Triangle...");
+                if (sideA == sideB && sideB == sideC && sideC == sideA)
+                {
+                    return "\nIt is an Equilateral Triangle\n";
+                }
+                if (sideA == sideB || sideB == sideC || sideC == sideA)
+                {
+                    return "\nIt is an Isosceles Triangle\n";
+                }
+                return "\nIt is an Scalene Triangle\n";
+            }
+            else
+            {
+                return "\nIt is not a triangle\n";
+            }
+
+        }
+    }
+}
+
+
